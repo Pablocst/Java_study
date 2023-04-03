7908bef1152e0f87b53e7f28595a5a5d74fb573f
Pablocst <pablocstredes@gmail.com>
2023-04-03 16:13:25 -0300

    DF-1 10th commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! sixth commit" );
+        System.out.println( "Hello World! 10th commit" );
     }
 }
